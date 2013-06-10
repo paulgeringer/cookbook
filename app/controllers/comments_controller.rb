21e86720581ba0832b8f782a4ae21dadcd6b5efa
Paul Geringer <paul.j.geringer@gmail.com>
2013-06-09 19:09:52 -0500

    Added comments, needs the basic security settings

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = @recipe.comments.create(params[:comment])
+    redirect_to recipe_path(@recipe)
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = @recipe.comments.find(params[:id])
+    @comment.destroy
+    redirect_to recipe_path(@recipe)
+  end
+
+end
---
+++ app/models/recipe.rb
@@ -0,0 +1,9 @@
+class Recipe < ActiveRecord::Base
+  attr_accessible :content, :name, :title
+
+  validates :name, :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+
+  has_many :comments, :dependent => :destroy
+end
